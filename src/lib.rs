87a40fb177248ca786fa6b34bb0ef1aef63aef98
Lars Isenberg <isenberg.lars@gmail.com>
2021-08-04 21:31:21 +0200

    Added folding as a default implementation for the signal trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,24 @@
 use std::ops::Index;
 
-trait Signal: Index<usize, Output=i32> {
+trait Signal: Index<i32, Output=i32> + Index<usize, Output=i32> + Sized + Clone {
     fn new(values: Vec<i32>) -> Self;
     fn len(&self) -> usize;
+    fn fold(&self, rhs: &Self) -> Self {
+        let n = self.len();
+        let m = rhs.len();
+        let mut output = Vec::new();
+        for i in 0..(n + m - 1) as i32 {
+            let mut sum = 0;
+            for j in 0..(m) as i32 {
+                sum += rhs[j] * self[i - j];
+            }
+            output.push(sum);
+        }
+        Self::new(output)
+    }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 struct AperiodicSignal {
     values: Vec<i32>,
 }
@@ -28,6 +41,18 @@ impl Index<usize> for AperiodicSignal {
             &0
         } else {
             &self.values[index]
+        }
+    }
+}
+
+impl Index<i32> for AperiodicSignal {
+    type Output = i32;
+
+    fn index(&self, index: i32) -> &Self::Output {
+        if index < 0 || index >= self.len() as i32 {
+            &0
+        } else {
+            &self.values[index as usize]
         }
     }
 }
@@ -143,5 +168,26 @@ mod tests {
         let even = AperiodicSignal::new(vec![4, 4, 1, 3, 3, 1, 4]);
         let odd = AperiodicSignal::new(vec![0, -3, -4, -7, 7, 4, 3]);
         assert_eq!(even_odd_decomposition(sig), vec![even, odd]);
+    }
+
+    #[test]
+    fn fold_identity() {
+        let signal = AperiodicSignal::new(vec![1, 2, 3, 4, 5]);
+        let kernel = AperiodicSignal::new(vec![1]);
+        assert_eq!(signal.fold(&kernel), signal);
+    }
+
+    #[test]
+    fn fold_delay() {
+        let signal = AperiodicSignal::new(vec![1, 2, 3, 4, 5]);
+        let kernel = AperiodicSignal::new(vec![0, 0, 1]);
+        assert_eq!(signal.fold(&kernel), AperiodicSignal::new(vec![0, 0, 1, 2, 3, 4, 5]));
+    }
+
+    #[test]
+    fn fold_amplify() {
+        let signal = AperiodicSignal::new(vec![1, 2, -3, 4, 5]);
+        let kernel = AperiodicSignal::new(vec![2]);
+        assert_eq!(signal.fold(&kernel), AperiodicSignal::new(vec![2, 4, -6, 8, 10]));
     }
 }
