78a2f82d07843a837fcfcd18089b57c5a5e7ae26
Lars Isenberg <isenberg.lars@gmail.com>
2021-08-07 19:24:46 +0200

    Added first implementation for calculating the real number discrete fourier transformation

--- src/lib.rs
+++ src/lib.rs
@@ -34,15 +34,61 @@ pub fn even_odd_decomposition<S: Signal>(signal: S) -> Vec<S> {
     
     let mut odd = Vec::new();
     odd.reserve_exact(signal.len() + 1);
-    odd.push(0);
+    odd.push(0.0);
 
     for i in 1..signal.len() {
         let front_index = i % signal.len();
         let back_index = (signal.len() - i) % signal.len();
-        even.push((signal[front_index] + signal[back_index]) / 2);
-        odd.push((signal[front_index] - signal[back_index]) / 2);
+        even.push((signal[front_index] + signal[back_index]) / 2.0);
+        odd.push((signal[front_index] - signal[back_index]) / 2.0);
     }
     vec![S::new(even), S::new(odd)]
+}
+
+struct RealDft {
+    cos_amplitude: AperiodicSignal,
+    sin_amplitude: AperiodicSignal,
+}
+
+impl RealDft {
+    fn new<S:Signal>(signal: S) -> Self {
+        let mut cos_vec = Vec::new();
+        let mut sin_vec = Vec::new();
+        for k in 0..(signal.len() / 2 + 1) {
+            let mut cos_sum = 0.0;
+            let mut sin_sum = 0.0;
+            for n in 0..signal.len() + 1 {
+                cos_sum += signal[n] * RealDft::base_cos(k, n, signal.len());
+                sin_sum += signal[n] * RealDft::base_sin(k, n, signal.len());
+            }
+            cos_vec.push(cos_sum);
+            sin_vec.push(sin_sum);
+        }
+        let cos_amplitude = AperiodicSignal::new(cos_vec);
+        let sin_amplitude = AperiodicSignal::new(sin_vec);
+        RealDft { cos_amplitude, sin_amplitude }
+    }
+
+    pub fn cos_amplitude(&self) -> &AperiodicSignal {
+        &self.cos_amplitude
+    }
+
+    pub fn sin_amplitude(&self) -> &AperiodicSignal {
+        &self.sin_amplitude
+    }
+
+    fn base_cos(k: usize, n: usize, length: usize) -> f64 {
+        let k: f64 = k as f64;
+        let n: f64 = n as f64;
+        let length: f64 = length as f64;
+        (2.0*PI*k*n/length).cos()
+    }
+    fn base_sin(k: usize, n: usize, length: usize) -> f64 {
+        let k: f64 = k as f64;
+        let n: f64 = n as f64;
+        let length: f64 = length as f64;
+        (2.0*PI*k*n/length).sin()
+    }
 }
 
 #[cfg(test)]
@@ -88,6 +134,14 @@ mod tests {
         let even = AperiodicSignal::new(vec![4.0, 4.0, 1.0, 3.0, 3.0, 1.0, 4.0]);
         let odd = AperiodicSignal::new(vec![0.0, -3.0, -4.0, -7.0, 7.0, 4.0, 3.0]);
         assert_eq!(even_odd_decomposition(sig), vec![even, odd]);
+    }
+
+    #[test]
+    fn real_dft_even_length() {
+        let sig = AperiodicSignal::new(vec![4.0, 1.0, -5.0, -4.0]);
+        let dft = RealDft::new(sig);
+        assert_eq!(dft.cos_amplitude(), &AperiodicSignal::new(vec![-4.0, 9.0, 2.0]));
+        //assert_eq!(dft.sin_amplitude(), &AperiodicSignal::new(vec![0.0, 5.0, 0.0]));
     }
 
 }
